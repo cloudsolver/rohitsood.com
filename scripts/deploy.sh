b731b7bf581ffa4df5f30fab992f3853e26bb28b
Ray Sood CloudSolver <digitaldivvy@gmail.com>
2023-05-11 12:04:48 -0400

    deploy script and profile image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BUCKET_NAME="rohitsood.com"
 echo "Pushing contents of ../www to the AWS Cloud on s3://${BUCKET_NAME}"
-aws s3 sync ../www "s3://${BUCKET_NAME}" \
+aws s3 sync ../ "s3://${BUCKET_NAME}" \
     --exclude ".*" \
     --exclude "*.sh"
 
